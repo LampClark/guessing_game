52594e6bde741b5fca0dcd6807028d5ccb9590e5
Charles Lee <unknownsanon@gmail.com>
2023-08-16 10:54:00 +0800

    saving

--- guess.rb
+++ guess.rb
@@ -2,28 +2,48 @@ puts "Hi, What's your name!"
 name = gets.chomp
 puts "Hello #{name}, try guess the secret number"
 
-secret_number = rand(1..100)
-# puts secret number
-counter = 1
-chances = 6
-guesses = []
+def play_game
+  secret_number = rand(1..100)
+  counter = 1
+  chances = 6
+  guesses = []
 
-while counter <= chances
-  puts secret_number
+  while counter <= chances
+    puts "Choose a number between 1 to 100"
+    guesses_number = gets.to_i
 
-puts "Choose a number between 1 to 100"
-guesses_number = gets.to_i
-puts guesses_number
+  if guesses_number > secret_number
+    puts "Number is too high"
+  elsif guesses_number < secret_number
+    puts "Number is too low"
+  else
+    puts "Congratulation, you found the correct number (#{secret_number})!"
+    break
+  end
 
-if guesses_number > secret_number
-  puts "Number is too high"
-elsif guesses_number < secret_number
-  puts "Number is too low"
-else
-  puts "You found the correct number"
-  break
+    guesses << guesses_number
+    puts "Guesses: #{guesses.join('. ')}"
+
+    if counter < chances
+      puts "You have #{chances - counter} #{chances - counter == 1 ? 'chance' : 'chance'} left"
+    else
+      puts "Sorry, you're out of chances. The secret was #{secret_number}."
+    end
+
+    counter +=1
+  end
 end
-guesses << guesses_number
-puts "guesses: #{guesses}"
-counter +=1
+
+
+play_game
+
+ loop do
+  puts "Do you want to try again? (yes/no)"
+  play_again = gets.chomp.downcase
+  if play_again == "yes"
+    play_game
+  else
+    puts "thank you for playing!!!"
+    break
+  end
 end
